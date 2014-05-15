76037e7774496d147e6a803705a1fdd66990d8df
jhong01 <jhong01@email.wm.edu>
2014-05-15 16:03:20 -0400

    Initial commit

---
+++ android/src/com/hsindustries/ctfpro/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.hsindustries.ctfpro.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hsindustries.ctfpro.CTFLauncher;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CTFLauncher(), config);
+	}
+}
